eaecbe3ea354cefbdfa6e2f4dbb4e6c6686c1e9c
divyamagwl <agrawaldivyam2001@gmail.com>
2021-11-24 15:42:20 +0530

    Added program test and Jenkinsfile

---
+++ program.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Program for calculating multiplication of two numbers
+
+def multiply(num1, num2):
+    return num1*num2
---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Test case for calculating mean of list of numbers
+
+import unittest
+from program import multiply
+
+class TestMean(unittest.TestCase):
+    def test_mul1(self):
+        result1 = multiply(12, 12)
+        self.assertEqual(result1, 144)
+    def test_mul2(self):
+        result2 = multiply(10, 0)
+        self.assertEqual(result2, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
